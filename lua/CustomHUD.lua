ab0bf0597edc8636fa054e0602a2b6c9118254c5
Kamikaze94 <kamikaze1337@live.de>
2016-03-26 05:42:22 +0100

    CustomHUD now usable with Maniac Perk

--- lua/CustomHUD.lua
+++ lua/CustomHUD.lua
@@ -1,4 +1,77 @@
-if WolfHUD and not WolfHUD.settings.use_customhud then return end
+if WolfHUD and not WolfHUD.settings.use_customhud then
+	if RequiredScript == "lib/managers/hudmanagerpd2" then
+
+		local set_stamina_value_original = HUDManager.set_stamina_value
+		local set_max_stamina_original = HUDManager.set_max_stamina
+		
+		function HUDManager:set_stamina_value(value, ...)
+			self._teammate_panels[HUDManager.PLAYER_PANEL]:set_current_stamina(value)
+			return set_stamina_value_original(self, value, ...)
+		end
+
+		function HUDManager:set_max_stamina(value, ...)
+			self._teammate_panels[HUDManager.PLAYER_PANEL]:set_max_stamina(value)
+			return set_max_stamina_original(self, value, ...)
+		end
+
+	elseif RequiredScript == "lib/managers/hud/hudteammate" then
+
+		local init_original = HUDTeammate.init
+
+		function HUDTeammate:init(i, ...)
+			init_original(self, i, ...)
+			
+			if i == HUDManager.PLAYER_PANEL and not HUDManager.CUSTOM_TEAMMATE_PANEL then
+				self:_create_stamina_circle()
+			end
+		end
+		
+		function HUDTeammate:_create_stamina_circle()
+			local radial_health_panel = self._panel:child("player"):child("radial_health_panel")
+			
+			self._stamina_bar = radial_health_panel:bitmap({
+				name = "radial_stamina",
+				texture = "guis/textures/pd2/hud_shield",
+				texture_rect = { 0, 0, 64, 64 },
+				render_template = "VertexColorTexturedRadial",
+				blend_mode = "add",
+				alpha = 1,
+				w = radial_health_panel:w() * 0.53,
+				h = radial_health_panel:h() * 0.53,
+				layer = 5
+			})
+			self._stamina_bar:set_color(Color(1, 1, 0, 0))
+			self._stamina_bar:set_center(radial_health_panel:child("radial_health"):center())
+			
+			self._stamina_line = radial_health_panel:rect({
+				color = Color.red:with_alpha(0.4),
+				w = radial_health_panel:w() * 0.05,
+				h = 2,
+				layer = 10,
+			})
+			self._stamina_line:set_center(radial_health_panel:child("radial_health"):center())
+		end
+
+		function HUDTeammate:set_max_stamina(value)
+			self._max_stamina = value
+			local radial_health_panel = self._panel:child("player"):child("radial_health_panel")
+			local w = self._stamina_bar:w()
+			local threshold = tweak_data.player.movement_state.stamina.MIN_STAMINA_THRESHOLD
+			local angle = 360 * (threshold/self._max_stamina) - 90
+			local x = 0.48 * w * math.cos(angle) + w * 0.5 + self._stamina_bar:x()
+			local y = 0.48 * w * math.sin(angle) + w * 0.5 + self._stamina_bar:y()
+			self._stamina_line:set_x(x)
+			self._stamina_line:set_y(y)
+			self._stamina_line:set_rotation(angle)
+		end
+
+		function HUDTeammate:set_current_stamina(value)
+			self._stamina_bar:set_color(Color(1, value/self._max_stamina, 0, 0))
+		end
+		
+	end
+	return
+end
 if string.lower(RequiredScript) == "lib/managers/hudmanagerpd2" then
 	HUDManager.CUSTOM_TEAMMATE_PANEL = true	--External flag
 	HUDManager._USE_BURST_MODE = HUDManager._USE_BURST_MODE or false	--Updated on burst fire plugin load
@@ -162,6 +235,7 @@ elseif string.lower(RequiredScript) == "lib/managers/hud/hudteammate" then
 	function HUDTeammate:animate_update_absorb(...) end
 	function HUDTeammate:animate_update_absorb_active(...) end
 	function HUDTeammate:animate_update_absorb_max(...) end
+	function HUDTeammate:set_info_meter(...) end
 
 	HUDTeammate._PLAYER_PANEL_SCALE = WolfHUD.settings.PLAYER_PANEL_SCALE or 0.85
 	HUDTeammate._TEAMMATE_PANEL_SCALE = WolfHUD.settings.TEAMMATE_PANEL_SCALE or 0.75
