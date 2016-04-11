d627ab95017a9219f81de6727f661d8747457e57
Kamikaze94 <kamikaze1337@live.de>
2016-04-11 18:09:07 +0200

    Unused code removal.

--- lua/Scripts.lua
+++ lua/Scripts.lua
@@ -1417,18 +1417,4 @@ elseif string.lower(RequiredScript) == "lib/tweak_data/timespeedeffecttweakdata"
 			sync = true
 		}
 	end
-elseif string.lower(RequiredScript) == "lib/managers/objectinteractionmanager" then
-	ObjectInteractionManager.AUTO_PICKUP_DELAY = 0.3 --Delay in seconds between auto-pickup procs (0 -> as fast as possible)
-	local _update_targeted_original = ObjectInteractionManager._update_targeted
-	function ObjectInteractionManager:_update_targeted(...)
-		_update_targeted_original(self, ...)
-
-		if alive(self._active_unit) then
-			local t = Application:time()
-			if self._active_unit:base() and self._active_unit:base().small_loot and managers.menu:get_controller():get_input_bool("interact") and (t >= (self._next_auto_pickup_t or 0)) then
-				self._next_auto_pickup_t = t + ObjectInteractionManager.AUTO_PICKUP_DELAY
-				self:interact(managers.player:player_unit())
-			end
-		end
-	end
 end
