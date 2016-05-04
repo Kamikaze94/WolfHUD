a7f5d93d331419757d3b55dd103ef99b6971f9fe
Kamikaze94 <kamikaze1337@live.de>
2016-05-04 23:50:46 +0200

    Removed Hook for KickPlayer node.

--- lua/MenuTweaks.lua
+++ lua/MenuTweaks.lua
@@ -1,26 +1,5 @@
 if string.lower(RequiredScript) == "lib/managers/menumanager" then
 	function MenuCallbackHandler:is_dlc_latest_locked(check_dlc) return false end
-
-	Hooks:PostHook( KickPlayer, "modify_node", "WoldHUD_KickPlayerPostModify", function( self, node, up )
-		local new_node = deep_clone( node )
-		if managers.network:session() then
-			for __,peer in pairs( managers.network:session():peers() ) do
-				local params = {
-					name			= peer:name(),
-					text_id			= ( peer:name() .. " (" .. (peer:rank() > 0 and managers.experience:rank_string(peer:rank()) .. "-" or "") .. peer:level() .. ")" ),
-					callback		= 'kick_player',
-					to_upper		= false,
-					localize		= 'false',
-					rpc				= peer:rpc(),
-					peer			= peer,
-				}
-				local new_item = node:create_item( nil, params )
-				new_node:add_item( new_item )
-			end
-		end
-		managers.menu:add_back_button( new_node )
-		return new_node
-	end)
 elseif string.lower(RequiredScript) == "lib/managers/menu/blackmarketgui" then 
 	-- Show Mod Icons of all Mods in Inventory Boxxes
 	local orig_blackmarket_gui_slot_item_select = BlackMarketGuiSlotItem.select
