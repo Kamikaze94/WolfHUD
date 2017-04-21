9e529b6d9933a403f7360b919684ffdbdec5b25a
BangL <henno.rickowski@gmail.com>
2017-04-21 19:33:20 +0200

    fix: actually disable "crew loadout in tab stats panel" when using LPI as intended

--- lua/EnhancedCrewLoadout.lua
+++ lua/EnhancedCrewLoadout.lua
@@ -587,7 +587,7 @@ elseif string.lower(RequiredScript) == "lib/managers/hud/hudstatsscreen" then
 	function HUDStatsScreen:init(...)
 		init_original(self, ...)
 
-		self._USING_CREW_LOADOUT = WolfHUD:getSetting({"CrewLoadout", "SHOW_ON_STATS_PANEL"}, true)
+		self._USING_CREW_LOADOUT = WolfHUD:getSetting({"CrewLoadout", "SHOW_ON_STATS_PANEL"}, true) and (not _G.LobbyPlayerInfo or not LobbyPlayerInfo.settings.show_skills_in_stats_screen)
 		local right_panel = self._full_hud_panel:child("right_panel")
 		if alive(right_panel) and self._USING_CREW_LOADOUT then
 			self._loadout_data = right_panel:panel({
@@ -690,7 +690,7 @@ elseif string.lower(RequiredScript) == "lib/managers/hud/hudstatsscreen" then
 	end
 
 	function HUDStatsScreen:update_loadout_panel(peer_id)
-		if self._peer_loadout[peer_id] and (not _G.LobbyPlayerInfo or not LobbyPlayerInfo.settings.show_skills_in_stats_screen) then
+		if self._peer_loadout[peer_id] then
 			local outfit
 			if self._peer_loadout[peer_id]:local_peer() then
 				outfit = managers.blackmarket:unpack_outfit_from_string(managers.blackmarket:outfit_string())
