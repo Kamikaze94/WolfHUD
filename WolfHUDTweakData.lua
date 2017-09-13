44bef9a844905cd5f44cb4a2a5b4c47a458732a5
BreakinBenny <fox@blaj.net>
2017-09-13 21:18:21 +0200

    A little less indenting

--- WolfHUDTweakData.lua
+++ WolfHUDTweakData.lua
@@ -1,18 +1,18 @@
 WolfHUDTweakData = WolfHUDTweakData or class()
 function WolfHUDTweakData:init()
 	----------------------------------------------------------------------------------------------------------------------
-	-- WolfHUD Tweak Data																								--
+	-- WolfHUD Tweak Data
 	----------------------------------------------------------------------------------------------------------------------
-	-- This file enables access to advanced settings, or those I cannot really implement into ingame menus easily.		--
-	-- If you want to save those changes, please copy this file to "Payday 2/mods/saves" and edit that copy instead.	--
-	-- You will need to take care of that version beeing up to date on your own. 										--
-	-- It will not get changed on updates automatically.																--
-	-- If you encounter problems, make sure the contents of this file matches the contents of your customized version.	--
+	-- This file enables access to advanced settings, or those I cannot really implement into ingame menus easily.
+	-- If you want to save those changes, please copy this file to "Payday 2/mods/saves" and edit that copy instead.
+	-- You will need to take care of that version beeing up to date on your own.
+	-- It will not get changed on updates automatically.
+	-- If you encounter problems, make sure the contents of this file matches the contents of your customized version.
 	----------------------------------------------------------------------------------------------------------------------
 
 	-- Currency used ingame
-	self.CASH_SIGN = "$"				-- Dollar
-	--self.CASH_SIGN = "\194\128"		-- EUR
+	self.CASH_SIGN = "$"	-- Dollar
+	--self.CASH_SIGN = "\194\128"	-- EUR
 
 	-- Maximum amount of Plans, that can be saved per level/map.
 	self.MAX_PRE_PLANS = 10
@@ -36,79 +36,79 @@ function WolfHUDTweakData:init()
 	-- The total width and height of those panels are fixed, so adding too many components into a row or column will make them incredibly small.
 	-- Available components:
 --[[
-		name 					-> The name (+ rank in lobby)
-		level 					-> The level + infamy
-		ping					-> The latency
-		playtime 				-> The Steam playtime (in hours)
-		character				-> The used character
-		skills 					-> The equipped skill build
-		perk 					-> The equipped perkdeck
-		primary 				-> The primary weapon + used mods
-		secondary 				-> The secondary weapon + used mods
-		melee_weapon			-> The equipped melee weapon
-		grenade 				-> The eqipped throwable
-		mask					-> The mask worn
-		armor 					-> The armor worn
-		deployable 				-> The equipped deployable
+		name 		-> The name (+ rank in lobby)
+		level 		-> The level + infamy
+		ping		-> The latency
+		playtime 	-> The Steam playtime (in hours)
+		character	-> The used character
+		skills 		-> The equipped skill build
+		perk 		-> The equipped perkdeck
+		primary 	-> The primary weapon + used mods
+		secondary 	-> The secondary weapon + used mods
+		melee_weapon	-> The equipped melee weapon
+		grenade 	-> The eqipped throwable
+		mask		-> The mask worn
+		armor 		-> The armor worn
+		deployable 	-> The equipped deployable
 		secondary_deployable 	-> The eqipped secondary deployable (in case of Jack of all trades)
 --]]
 	self.STD_LOBBY_LOADOUT_LAYOUT = {
-										{ "playtime", "ping" },
-										{ "name" },
-										{ "character" },
-										{ "skills" },
-										{ "perk" },
-										{ "primary" },
-										{ "secondary" },
-										{ "melee_weapon" },
-										{ "grenade", "armor" },
-										{ "deployable", "secondary_deployable" }
-									}
+						{ "playtime", "ping" },
+						{ "name" },
+						{ "character" },
+						{ "skills" },
+						{ "perk" },
+						{ "primary" },
+						{ "secondary" },
+						{ "melee_weapon" },
+						{ "grenade", "armor" },
+						{ "deployable", "secondary_deployable" }
+					}
 	self.CS_LOBBY_LOADOUT_LAYOUT = {
-										{ "playtime", "ping" },
-										{ "name" },
-										{ "skills" },
-										{ "perk" },
-										{ "primary", "secondary" },
-										{ "grenade", "armor" },
-										{ "deployable", "secondary_deployable" }
-									}
-	self.BRIEFING_LOADOUT_LAYOUT = 	{
-										{ "perk" },
-										{ "skills" },
-										{ "primary" },
-										{ "secondary" },
-										{ "melee_weapon", "grenade" },
-										{ "armor", "mask" },
-										{ "deployable", "secondary_deployable" }
-									}
-	self.TAB_LOADOUT_LAYOUT = 		{
-										{ "name", "ping" },
-										{ "skills", "perk" },
-									}
+						{ "playtime", "ping" },
+						{ "name" },
+						{ "skills" },
+						{ "perk" },
+						{ "primary", "secondary" },
+						{ "grenade", "armor" },
+						{ "deployable", "secondary_deployable" }
+					}
+	self.BRIEFING_LOADOUT_LAYOUT = {
+						{ "perk" },
+						{ "skills" },
+						{ "primary" },
+						{ "secondary" },
+						{ "melee_weapon", "grenade" },
+						{ "armor", "mask" },
+						{ "deployable", "secondary_deployable" }
+				}
+	self.TAB_LOADOUT_LAYOUT = {
+					{ "name", "ping" },
+					{ "skills", "perk" },
+				}
 
 	-- Color table
-	-- 		Add or remove any color you want
-	--		'color' needs to be that colors hexadecimal code
-	-- 		'name' will be the name it appears in the selection menus
+	-- 	Add or remove any color you want
+	--	'color' needs to be that colors hexadecimal code
+	-- 	'name' will be the name it appears in the selection menus
 	self.color_table = {
-		{ color = 'FFFFFF', name = "white" 			},
+		{ color = 'FFFFFF', name = "white" 	},
 		{ color = 'F2F250', name = "light_yellow" 	},
 		{ color = 'F2C24E', name = "light_orange" 	},
-		{ color = 'E55858', name = "light_red" 		},
+		{ color = 'E55858', name = "light_red" 	},
 		{ color = 'CC55CC', name = "light_purple" 	},
 		{ color = '00FF00', name = "light_green" 	},
-		{ color = '00FFFF', name = "light_blue" 	},
-		{ color = 'BABABA', name = "light_gray" 	},
-		{ color = 'FFFF00', name = "yellow" 		},
-		{ color = 'FFA500', name = "orange" 		},
-		{ color = 'FF0000', name = "red" 			},
-		{ color = '800080', name = "purple" 		},
-		{ color = '008000', name = "green" 			},
-		{ color = '0000FF', name = "blue" 			},
-		{ color = '808080', name = "gray" 			},
-		{ color = '000000', name = "black" 			},
-		{ color = nil, name = "rainbow" 			},
+		{ color = '00FFFF', name = "light_blue" },
+		{ color = 'BABABA', name = "light_gray" },
+		{ color = 'FFFF00', name = "yellow" 	},
+		{ color = 'FFA500', name = "orange" 	},
+		{ color = 'FF0000', name = "red" 	},
+		{ color = '800080', name = "purple" 	},
+		{ color = '008000', name = "green" 	},
+		{ color = '0000FF', name = "blue" 	},
+		{ color = '808080', name = "gray" 	},
+		{ color = '000000', name = "black" 	},
+		{ color = nil, name = "rainbow" 	},
 	}
 
 	self:post_init()
