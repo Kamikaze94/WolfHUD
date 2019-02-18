3534292e9d5e564a0075b8c046300f385973b95b
Henno Rickowski <henno.rickowski@gmail.com>
2019-02-18 14:06:17 +0100

    Gadgets: fix laser menu callback checks

--- Core.lua
+++ Core.lua
@@ -769,7 +769,7 @@ if not _G.WolfHUD then
 				end
 			end,
 			["GADGETS"] = function(setting, value)
-				if managers.hud and managers.hud.change_hud_setting and #setting >= 4 then
+				if WeaponGadgetBase and WeaponGadgetBase.update_theme_setting and #setting >= 4 then
 					WeaponGadgetBase.update_theme_setting(setting[1], setting[2], setting[3], setting[4], WolfHUD:getColor(value) or value)
 				end
 			end,
