833cdf5a2c8fa97c5ef14c4ced45d3729ee2db20
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2019-11-09 01:09:40 +0000

    Added item to function

--- lua/PrePlanManager.lua
+++ lua/PrePlanManager.lua
@@ -672,8 +672,8 @@ elseif requiredScript == "lib/managers/preplanningmanager" then
 	end
 elseif requiredScript == "lib/managers/menu/preplanningmapgui" then
 	local set_map_position_to_item_original = PrePlanningMapGui.set_map_position_to_item
-	function PrePlanningMapGui:set_map_position_to_item(...)
+	function PrePlanningMapGui:set_map_position_to_item(item, ...)
 	if item then
-		return set_map_position_to_item_original(self, ...)
+		return set_map_position_to_item_original(self, item, ...)
 	end
 end
